6929f09d92bcea087820f83bf888cefd59b2a19b
huntercront <38252870+huntercront@users.noreply.github.com>
2021-06-20 13:56:36 +0300

    add anim functions

--- app.js
+++ app.js
@@ -1,3 +1,69 @@
+let slideUp = (target, duration = 500) => {
+    target.style.transitionProperty = 'height, margin, padding';
+    target.style.transitionDuration = duration + 'ms';
+    target.style.boxSizing = 'border-box';
+    target.style.height = target.offsetHeight + 'px';
+    target.offsetHeight;
+    target.style.overflow = 'hidden';
+    target.style.height = 0;
+    target.style.paddingTop = 0;
+    target.style.paddingBottom = 0;
+    target.style.marginTop = 0;
+    target.style.marginBottom = 0;
+    window.setTimeout(() => {
+        target.style.display = 'none';
+        target.style.removeProperty('height');
+        target.style.removeProperty('padding-top');
+        target.style.removeProperty('padding-bottom');
+        target.style.removeProperty('margin-top');
+        target.style.removeProperty('margin-bottom');
+        target.style.removeProperty('overflow');
+        target.style.removeProperty('transition-duration');
+        target.style.removeProperty('transition-property');
+        //alert("!");
+    }, duration);
+}
+
+let slideDown = (target, duration = 500) => {
+    target.style.removeProperty('display');
+    let display = window.getComputedStyle(target).display;
+
+    if (display === 'none')
+        display = 'block';
+
+    target.style.display = display;
+    let height = target.offsetHeight;
+    target.style.overflow = 'hidden';
+    target.style.height = 0;
+    target.style.paddingTop = 0;
+    target.style.paddingBottom = 0;
+    target.style.marginTop = 0;
+    target.style.marginBottom = 0;
+    target.offsetHeight;
+    target.style.boxSizing = 'border-box';
+    target.style.transitionProperty = "height, margin, padding";
+    target.style.transitionDuration = duration + 'ms';
+    target.style.height = height + 'px';
+    target.style.removeProperty('padding-top');
+    target.style.removeProperty('padding-bottom');
+    target.style.removeProperty('margin-top');
+    target.style.removeProperty('margin-bottom');
+    window.setTimeout(() => {
+        target.style.removeProperty('height');
+        target.style.removeProperty('overflow');
+        target.style.removeProperty('transition-duration');
+        target.style.removeProperty('transition-property');
+    }, duration);
+}
+var slideToggle = (target, duration = 500) => {
+    if (window.getComputedStyle(target).display === 'none') {
+        return slideDown(target, duration);
+    } else {
+        return slideUp(target, duration);
+    }
+}
+
+
 let width = document.querySelector('#width')
 let height = document.querySelector('#height')
 let error = document.querySelector('.error')
@@ -10,11 +76,13 @@ var sum = 0;
 console.log(sectionTwo.checked);
 calc.addEventListener('click', function() {
     if (width.value == '' || height.value == '') {
-        error.classList.add('error-show')
+        // error.classList.add('error-show')
+        slideToggle(error, 300)
         if (width.value == '') { width.classList.add('error-red') }
         if (height.value == '') { height.classList.add('error-red') }
         setTimeout(function() {
-            error.classList.remove('error-show')
+            slideToggle(error, 300)
+                // error.classList.remove('error-show')
             width.classList.remove('error-red')
             height.classList.remove('error-red')
         }, 3000)
@@ -31,6 +99,7 @@ calc.addEventListener('click', function() {
         let result = document.querySelector('.result-text');
         result.textContent = sum + ' руб.';
         let res = document.querySelector('.result');
-        res.classList.add('result-show');
+        // res.classList.add('result-show');
+        slideToggle(res, 300)
     }
 })
